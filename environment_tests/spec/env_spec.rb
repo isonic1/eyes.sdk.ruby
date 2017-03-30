95516231426aa9487a72cff4879e043ea5b8cc66
Nikita Andreyev <nsa31415@gmail.com>
2017-03-30 17:50:32 +0300

    parallelizing automated tests

--- environment_tests/spec/env_spec.rb
+++ environment_tests/spec/env_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 require 'docker_files_list'
 
-RSpec.describe 'Environment tests' do
-  include Rspec::Shell::Expectations
+DockerFilesList.dockerfiles do |dockerfile, _dir|
+  RSpec.describe "Environment tests #{dockerfile}" do
+    include Rspec::Shell::Expectations
 
-  let(:stubbed_env) { create_stubbed_env }
-
-  DockerFilesList.dockerfiles do |dockerfile, _dir|
-    it dockerfile do
+    let(:stubbed_env) { create_stubbed_env }
+    it "ruby #{dockerfile}" do
       _stdout, _stderr, status = stubbed_env.execute(
-        "docker run  -v $PWD/../:/source_dir -w /workdir #{dockerfile} " \
-        "/source_dir/environment_tests/spec/scripts/prepare_repo_and_run_tests.sh > log/#{dockerfile}.log 2>&1"
+          "docker run  --rm -v $PWD/../:/source_dir -w /workdir #{dockerfile} " \
+      "/source_dir/environment_tests/spec/scripts/prepare_repo_and_run_tests.sh > log/#{dockerfile}.log 2>&1"
       )
       expect(status.exitstatus).to eq 0
     end
