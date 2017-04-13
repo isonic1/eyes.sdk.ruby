3d3f69cf8c3a3cc83d075fab4d1e24a46c7f056a
Nikita Andreyev <nsa31415@gmail.com>
2017-04-13 20:03:07 +0300

    eyes.disable_horizontal_scrolling flag has been implemented eyes.disable_vertical_scrolling flag has been implemented

--- lib/applitools/selenium/scroll_position_provider.rb
+++ lib/applitools/selenium/scroll_position_provider.rb
@@ -5,8 +5,10 @@ module Applitools::Selenium
 
     def_delegators 'Applitools::EyesLogger', :logger, :log_handler, :log_handler=
 
-    def initialize(executor)
+    def initialize(executor, disable_horizontal = false, disable_vertical = false)
       self.executor = executor
+      self.disable_horizontal = disable_horizontal
+      self.disable_vertical = disable_vertical
     end
 
     # The scroll position of the current frame
@@ -36,8 +38,11 @@ module Applitools::Selenium
     alias scroll_to position=
 
     def entire_size
+      viewport_size = Applitools::Utils::EyesSeleniumUtils.extract_viewport_size(executor)
       result = Applitools::Utils::EyesSeleniumUtils.entire_page_size(executor)
       logger.info "Entire size: #{result}"
+      result.width = viewport_size.width if disable_horizontal
+      result.height = viewport_size.height if disable_vertical
       result
     end
 
@@ -47,6 +52,6 @@ module Applitools::Selenium
 
     private
 
-    attr_accessor :executor
+    attr_accessor :executor, :disable_horizontal, :disable_vertical
   end
 end
