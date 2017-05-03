bca781f89d15d95bef85a24b27fcb82bfa12bbf6
Nikita Andreyev <nsa31415@gmail.com>
2017-05-03 18:59:52 +0300

    WIP: unfinished changes

--- lib/applitools/core/match_window_data.rb
+++ lib/applitools/core/match_window_data.rb
@@ -1,5 +1,50 @@
 module Applitools
   class MatchWindowData
+    class << self
+      def default_data
+        {
+          'IgnoreMismatch' => false,
+          'MismatchWait' => 0,
+          'Options' => {
+            'Name' => nil,
+            'UserInputs' => [],
+            'ImageMatchSettings' => {
+              'MatchLevel' => 'None',
+              'SplitTopHeight' => 0,
+              'SplitBottomHeight' => 0,
+              'IgnoreCaret' => false,
+              'Exact' => {
+                'MinDiffIntensity' => 0,
+                'MinDiffWidth' => 0,
+                'MinDiffHeight' => 0,
+                'MatchThreshold' => 0
+              }
+            },
+            'IgnoreExpectedOutputSettings' => false,
+            'ForceMatch' => false,
+            'ForceMismatch' => false,
+            'IgnoreMatch' => false,
+            'IgnoreMismatch' => false,
+            'Trim' => {
+              'Enabled' => false,
+              'ForegroundIntensity' => 0,
+              'MinEdgeLength' => 0
+            }
+          },
+          'Id' => nil,
+          'UserInputs' => [],
+          'AppOutput' => {
+            'Screenshot64' => nil,
+            'ScreenshotUrl' => nil,
+            'Title' => nil,
+            'IsPrimary' => false,
+            'Elapsed' => 0
+          },
+          'Tag' => nil
+        }
+      end
+    end
+
     attr_accessor :app_output, :user_inputs, :tag, :options, :ignore_mismatch
 
     def initialize(user_inputs, app_output, tag, ignore_mismatch, options = {})
@@ -13,6 +58,24 @@ module Applitools
     def screenshot
       app_output.screenshot.image.to_blob
     end
+
+    def ignore_mismatch=(value)
+
+    end
+
+    def tag=(value)
+      
+    end
+
+    def user_inputs=(value)
+
+    end
+
+    def app_output=(value)
+
+    end
+
+
 
     alias appOutput app_output
     alias userInputs user_inputs
@@ -29,6 +92,12 @@ module Applitools
 
     def to_s
       to_hash
+    end
+
+    private
+
+    def current_data
+      @current_data ||= self.class.default_data
     end
   end
 end
