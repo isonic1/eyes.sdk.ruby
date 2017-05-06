023e463a1cc44e13864c0a283bf80b522a9fb4eb
Daniel Puterman <daniel.puterman@applitools.com>
2017-05-06 04:05:05 +0300

    Updated images examples (added commented proxy line + begin/rescue where needed).

--- examples/images_example_script.rb
+++ examples/images_example_script.rb
@@ -8,20 +8,26 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 eyes = Applitools::Images::Eyes.new
 eyes.api_key = ENV['APPLITOOLS_API_KEY']
 eyes.log_handler = Logger.new(STDOUT)
+# eyes.proxy = Applitools::Connectivity::Proxy.new 'http://localhost:9999'
+
 
 viber_home_image_bytes = File.read('./images/viber-home.png', mode: 'rb')
 viber_home_image = Applitools::Screenshot.from_datastream viber_home_image_bytes
 
-eyes.test(app_name: 'Eyes.Java', test_name: 'home1') do
-  eyes.check_image(image_path: './images/viber-home.png')
-  eyes.check_region(
-    image_bytes: viber_home_image_bytes,
-    region: Applitools::Region.new(1773, 372, 180, 220),
-    tag: 'Bada region'
-  )
-  eyes.add_mouse_trigger(:click, Applitools::Region::EMPTY, Applitools::Location.new(1866, 500))
-  eyes.check_image(image_path: './images/viber-bada.png')
+begin
+  eyes.test(app_name: 'Eyes.Java', test_name: 'home1') do
+    eyes.check_image(image_path: './images/viber-home.png')
+    eyes.check_region(
+      image_bytes: viber_home_image_bytes,
+      region: Applitools::Region.new(1773, 372, 180, 220),
+      tag: 'Bada region'
+    )
+    eyes.add_mouse_trigger(:click, Applitools::Region::EMPTY, Applitools::Location.new(1866, 500))
+    eyes.check_image(image_path: './images/viber-bada.png')
+  end
+rescue
 end
+
 
 eyes.test(app_name: 'Eyes.Java', test_name: 'home2') do
   eyes.check_image(image: viber_home_image)
