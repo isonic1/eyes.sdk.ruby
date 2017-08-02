a5f8fc64d10146b5da985b05c32b13864d0ae183
Nikita Andreyev <nsa31415@gmail.com>
2017-08-02 18:00:25 +0300

    match_level issue

--- spec/eyes_images_spec.rb
+++ spec/eyes_images_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 RSpec.describe Applitools::Images::Eyes do
   let(:image) { ChunkyPNG::Image.new(5, 5) }
   let(:target) { Applitools::Images::Target.any(image) }
-  before(:each) { subject.default_match_settings[:match_level] = 'TEST' }
+  before(:each) { subject.default_match_settings = { :match_level => :layout } }
   describe ':check_image' do
     it 'passes match_level to base' do
       expect(subject).to receive(:check_window_base) do |*opts|
-        expect(opts.last.match_level).to eq 'TEST'
+        expect(opts.last.match_level).to eq 'Layout'
       end.and_return(Applitools::MatchResults.new)
       subject.check_image(tag: 'nil', image: image)
     end
@@ -16,7 +16,7 @@ RSpec.describe Applitools::Images::Eyes do
   describe ':check_region' do
     it 'passes match_level to base' do
       expect(subject).to receive(:check_window_base) do |*opts|
-        expect(opts.last.match_level).to eq 'TEST'
+        expect(opts.last.match_level).to eq 'Layout'
       end.and_return(Applitools::MatchResults.new)
       subject.check_region(tag: 'nil', image: image, region: Applitools::Region::EMPTY)
     end
