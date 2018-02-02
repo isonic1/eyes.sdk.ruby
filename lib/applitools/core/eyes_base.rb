15cf153c0e7d6743e33e2a5c61863369970b2e25
nikita-andreev <nsa31415@gmail.com>
2018-02-02 17:30:22 +0200

    Calabash: new test doesn\'t raise an error

--- lib/applitools/core/eyes_base.rb
+++ lib/applitools/core/eyes_base.rb
@@ -159,6 +159,10 @@ module Applitools
       running_session.nil? ? false : true
     end
 
+    def new_session?
+      running_session && running_session.new_session?
+    end
+
     def app_name
       !current_app_name.nil? && !current_app_name.empty? ? current_app_name : @app_name
     end
@@ -221,7 +225,8 @@ module Applitools
       logger.info "openBase(app_name: #{options[:app_name]}, test_name: #{options[:test_name]}," \
           " viewport_size: #{options[:viewport_size]})"
 
-      raise Applitools::EyesError.new 'API key is missing! Please set it using api_key=' if api_key.nil?
+      raise Applitools::EyesError.new 'API key is missing! Please set it using api_key=' if
+        api_key.nil? || (api_key && api_key.empty?)
 
       self.viewport_size = options[:viewport_size]
       self.session_type = options[:session_type]
