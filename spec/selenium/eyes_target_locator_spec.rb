218d7e6d0408bfaef2dfe2c276408313a097e8e9
nikita-andreev <nsa31415@gmail.com>
2018-05-24 19:07:39 +0300

    eyes_target_locator_spec has been updated due to chenges in Applitools::Selenium::Driver

--- spec/selenium/eyes_target_locator_spec.rb
+++ spec/selenium/eyes_target_locator_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe 'Applitools::Selenium::EyesTargetLocator' do
   context ':frame' do
     it 'accepts a string as an argument' do
       allow(driver).to receive(:find_elements).and_return([true])
+      allow(driver).to receive(:find_element)
       allow(native_target_locator).to receive(:frame)
       expect { subject.frame('frame1') }.to_not raise_error
     end
